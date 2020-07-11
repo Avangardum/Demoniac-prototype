f5177672caa99275d32329af008aaa1b447ea9ea
Silvian73 <yurykuz65@gmail.com>
2020-07-12 00:39:51 +0300

    Сделал шипы

---
+++ Assets/Scripts/SceneLoader.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine.SceneManagement;
+
+public static class SceneLoader
+{
+    public static void LoadScene(string name) => SceneManager.LoadScene(name);
+    public static void ReloadScene() => LoadScene(SceneManager.GetActiveScene().name);
+}
