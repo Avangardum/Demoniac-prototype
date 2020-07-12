640d67624f757c8bcc14b01faf9f92baebf19c17
Silvian73 <yurykuz65@gmail.com>
2020-07-12 15:30:04 +0300

    Сделал следование камеры за игроком

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    [SerializeField] private Transform followedObject;
+    [SerializeField] private Vector2 offset;
+    [SerializeField] private float interpolationPerSecond;
+
+    private void LateUpdate()
+    {
+        Vector3 position = transform.position;
+        float z = position.z;
+        position = Vector2.Lerp(position, (Vector2)followedObject.position + offset, interpolationPerSecond * Time.deltaTime);
+        position.z = z;
+        transform.position = position;
+    }
+}
