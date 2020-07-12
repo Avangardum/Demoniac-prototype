4336303c1595d10af2191f3068957e23f23a242a
Silvian73 <yurykuz65@gmail.com>
2020-07-12 19:16:43 +0300

    Испрравил камеру

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -8,11 +8,11 @@ public class CameraFollow : MonoBehaviour
     [SerializeField] private Vector2 offset;
     [SerializeField] private float interpolationPerSecond;
 
-    private void LateUpdate()
+    private void FixedUpdate()
     {
         Vector3 position = transform.position;
         float z = position.z;
-        position = Vector2.Lerp(position, (Vector2)followedObject.position + offset, interpolationPerSecond * Time.deltaTime);
+        position = Vector2.Lerp(position, (Vector2)followedObject.position + offset, interpolationPerSecond * Time.fixedDeltaTime);
         position.z = z;
         transform.position = position;
     }
